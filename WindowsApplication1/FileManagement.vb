f5beed29dd039cea017a047f1e318491174fcd85
Dan <thejedipantsniffer@gmail.com>
2017-03-06 18:18:50 +0000

    added window close on filemanagement

--- WindowsApplication1/FileManagement.vb
+++ WindowsApplication1/FileManagement.vb
@@ -2,4 +2,9 @@
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+    Private Sub formclose(sender As Object, e As EventArgs) Handles Me.FormClosed
+
+        MainMenu.Show()
+
+    End Sub
 End Class
