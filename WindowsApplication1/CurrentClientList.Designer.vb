9a4c7e2bfb2f4c32c205a7c68d439002a3a4de53
Kyle <kylepearson124@gmail.com>
2017-03-08 13:51:51 +0000

    Login button added w/ textbox for membeID

--- WindowsApplication1/CurrentClientList.Designer.vb
+++ WindowsApplication1/CurrentClientList.Designer.vb
@@ -31,6 +31,9 @@ Partial Class CurrentClientList
         Me.TimeArrived = New System.Windows.Forms.Label()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.LogoutBut = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
@@ -53,10 +56,10 @@ Partial Class CurrentClientList
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.WelcomeMessage.AutoSize = True
         Me.WelcomeMessage.BackColor = System.Drawing.Color.Transparent
-        Me.WelcomeMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.WelcomeMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.WelcomeMessage.Location = New System.Drawing.Point(260, 9)
         Me.WelcomeMessage.Name = "WelcomeMessage"
-        Me.WelcomeMessage.Size = New System.Drawing.Size(398, 31)
+        Me.WelcomeMessage.Size = New System.Drawing.Size(355, 29)
         Me.WelcomeMessage.TabIndex = 6
         Me.WelcomeMessage.Text = "Current Customers Logged In"
         '
@@ -65,7 +68,7 @@ Partial Class CurrentClientList
         Me.NumberofCustomers.AutoSize = True
         Me.NumberofCustomers.BackColor = System.Drawing.Color.Transparent
         Me.NumberofCustomers.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.NumberofCustomers.Location = New System.Drawing.Point(365, 57)
+        Me.NumberofCustomers.Location = New System.Drawing.Point(292, 48)
         Me.NumberofCustomers.Name = "NumberofCustomers"
         Me.NumberofCustomers.Size = New System.Drawing.Size(57, 63)
         Me.NumberofCustomers.TabIndex = 7
@@ -185,13 +188,41 @@ Partial Class CurrentClientList
         '
         'LogoutBut
         '
-        Me.LogoutBut.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LogoutBut.Location = New System.Drawing.Point(502, 57)
+        Me.LogoutBut.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LogoutBut.Location = New System.Drawing.Point(507, 84)
         Me.LogoutBut.Name = "LogoutBut"
-        Me.LogoutBut.Size = New System.Drawing.Size(156, 46)
+        Me.LogoutBut.Size = New System.Drawing.Size(108, 40)
         Me.LogoutBut.TabIndex = 13
         Me.LogoutBut.Text = "Logout"
         Me.LogoutBut.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(507, 41)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 40)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(363, 73)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(126, 20)
+        Me.TextBox1.TabIndex = 15
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(351, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(148, 17)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Input MemberID Below"
         '
         'CurrentClientList
         '
@@ -200,7 +231,10 @@ Partial Class CurrentClientList
         Me.AutoScroll = True
         Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.background
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(752, 738)
+        Me.ClientSize = New System.Drawing.Size(661, 738)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.LogoutBut)
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Controls.Add(Me.NumberofCustomers)
@@ -226,4 +260,7 @@ Partial Class CurrentClientList
     Friend WithEvents TimeArrived As Label
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
     Friend WithEvents LogoutBut As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- WindowsApplication1/CurrentClientList.vb
+++ WindowsApplication1/CurrentClientList.vb
@@ -14,4 +14,16 @@
     Private Sub LogoutBut_Click(sender As Object, e As EventArgs) Handles LogoutBut.Click
         NumberofCustomers.Text = NumberofCustomers.Text - 1
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
 End Class
