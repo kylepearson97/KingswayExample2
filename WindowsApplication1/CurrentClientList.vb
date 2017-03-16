b51d001837eb260c65652f22db0d29af92a98976
Sam <samuelmercer@googlemail.com>
2017-03-16 22:54:32 +0000

    Attempting log out functions

--- WindowsApplication1/CurrentClientList.vb
+++ WindowsApplication1/CurrentClientList.vb
@@ -3,12 +3,9 @@
         NumberofCustomers.Text = (CurrentClient.RowCount.ToString - 1)
         Me.MembersTableAdapter1.Fill(Me.KingswayFitnessDataSet1.Members)
     End Sub
-    Private Sub NumberofCustomers_Click(sender As Object, e As EventArgs) Handles NumberofCustomers.Click
-        NumberofCustomers.Text = NumberofCustomers.Text + +1
-    End Sub
     Private Sub formclose(sender As Object, e As EventArgs) Handles Me.Closing
 
-        My.Settings.NumberofCustomers = Me.NumberofCustomers.Text
+        NumberofCustomers.Text = (CurrentClient.RowCount.ToString - 1)
         MainMenu.Show()
     End Sub
 
@@ -16,16 +13,39 @@
 
     Private Sub TextBox1_KeyPress(sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox1.KeyPress
         If e.KeyChar = Microsoft.VisualBasic.ChrW(Keys.Return) Then
-            Dim MemID = TextBox1.Text
-            Dim rows() As DataRow = KingswayFitnessDataSet1.Members.Select("MemberID =" + MemID)
-            If rows.Length = 0 Or rows.Length > 1 Then
-                MsgBox("No File Found")
+            If CurrentClient.RowCount.ToString - 1 < 45 Then
+
+                Dim MemID = TextBox1.Text
+                Dim rows() As DataRow = KingswayFitnessDataSet1.Members.Select("MemberID =" + MemID)
+                If rows.Length = 0 Or rows.Length > 1 Then
+                    MsgBox("No File Found")
+                Else
+                    CurrentClient.Rows.Add(rows(0).Item("MemberID"), rows(0).Item("Forename"), rows(0).Item("Surname"), DateTime.Now.ToString("yyyy/MM/dd HH:mm"))
+                End If
+                NumberofCustomers.Text = (CurrentClient.RowCount.ToString - 1)
             Else
-                CurrentClient.Rows.Add(rows(0).Item("MemberID"), rows(0).Item("Forename"), rows(0).Item("Surname"), DateTime.Now.ToString("yyyy/MM/dd HH:mm"))
+                MsgBox("Gym Full")
             End If
-            NumberofCustomers.Text = (CurrentClient.RowCount.ToString - 1)
+
         End If
+
 
     End Sub
 
+    Private Sub CurrentClient_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles CurrentClient.CellContentClick
+
+        ' Dim connstring As String = My.Settings.KingswayFitnessConnectionString1
+        'Dim conn As New OleDb.OleDbConnection
+        'Dim cmd As New OleDb.OleDbCommand
+        'conn.ConnectionString = My.Settings.KingswayFitnessConnectionString1
+        'conn.Open()
+        'Dim Sql As String
+        'Sql = "INSERT INTO [Member Activity Log](MemberID , [Time Logged out] , [Time Logged in])  VALUES('" & CurrentClient.CurrentRow.Cells.Item(0).Value & "','" & CurrentClient.CurrentRow.Cells.Item(3).Value & "','" & DateTime.Now.ToString("yyyy/MM/dd HH:mm") & "')"
+        'cmd = New OleDb.OleDbCommand(Sql, conn)
+        'MsgBox(cmd.ExecuteNonQuery())
+        'conn.Close()
+        MsgBox("User Logged Out")
+        CurrentClient.Rows.Remove(CurrentClient.CurrentRow)
+        NumberofCustomers.Text = (CurrentClient.RowCount.ToString - 1)
+    End Sub
 End Class
